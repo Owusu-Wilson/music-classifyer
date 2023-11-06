d135ab404e814abc56eae338b56445f0ea413ad8
Owusu Wilson <owusuwilson980@gmail.com>
2023-11-06 14:01:27 +0000

    Project initialized

---
+++ main.py
@@ -0,0 +1,38 @@
+import pickle
+import streamlit as st
+
+
+# IMPORTING THE TRAINED MODEL 
+
+# Open the pickle file in binary read mode.
+with open("my_pickle_file.pkl", "rb") as f:
+    # Load the pickled object from the file.
+    model = pickle.load(f)
+
+# Close the pickle file.
+f.close()
+
+# Use the pickled object in your application.
+# print(my_object)
+
+# THE APP INTERFACE
+st.title("Music Genre Prediction")
+
+gender = st.selectbox("Select Gender", ['Male', 'Female'])
+# age = st.text_input("Age")
+age = st.slider("Select Age", min_value=18, max_value=70)
+
+submit = st.button("Predict")
+clear = st.button("Clear")
+pred_box = st.write()
+
+gender_bool = 1 if gender=='Male' else 0
+
+if submit:
+    
+    prediction = model.predict([[gender_bool, age]])
+    st.write(prediction)
+    st.toast("completed 1 prediction")
+    
+# if clear:
+#     pred_box = ''
